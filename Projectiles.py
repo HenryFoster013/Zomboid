70b19523c290e65500e1f82d32f7850fcbda0806
akafin <pedzinskifinley@gmail.com>
2025-02-18 20:03:47 +0000

    implemented wall collision class within projectiles class

--- Projectiles.py
+++ Projectiles.py
@@ -1,5 +1,6 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 import math
+from Tile import *
 
 class bullet:
 
@@ -26,8 +27,35 @@ class bullet:
     
     def Update(self):
         if(self.alive):
+            self.wall_collision_check()
             self.position[0] += self.direction[0] * self.speed * self.mult
             self.position[1] += self.direction[1] * self.speed * self.mult
             self.frame_counter += 1
             #if(self.frame_counter >= self.lifetime):
             #    self.destroyThis()
+    
+    def wall_collision_check(self):
+        global up_collision,down_collision,left_collision,right_collision
+        up_collision = False
+        down_collision = False
+        left_collision = False
+        right_collision = False
+        for i in range(len(walls)):
+            if walls[i].check_collision((self.position[0] + self.speed), self.position[1]) == True:
+                right_collision = True
+            if walls[i].check_collision((self.position[0] - self.speed), self.position[1]) == True:
+                left_collision = True
+            if walls[i].check_collision(self.position[0], (self.position[1] + self.speed)) == True:
+                down_collision = True
+            if walls[i].check_collision(self.position[0], (self.position[1] - 10)) == True:
+                up_collision = True
+
+        for i in range(len(diagonal_walls)):
+            if diagonal_walls[i].check_collision((self.position[0] + self.speed), self.position[1]) == True:
+                right_collision = True
+            if diagonal_walls[i].check_collision((self.position[0] - self.speed), self.position[1]) == True:
+                left_collision = True
+            if diagonal_walls[i].check_collision(self.position[0], (self.position[1] + self.speed)) == True:
+                down_collision = True
+            if diagonal_walls[i].check_collision(self.position[0], (self.position[1] - self.speed)) == True:
+                up_collision = True
